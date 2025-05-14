cbc4f9e6fc9fcf7c0811de722c0319edbaad50ea
DDefty <dawid.kogut99@gmail.com>
2025-05-14 10:54:53 +0200

    Add tests for login validation: handle missing email and password

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -14,4 +14,18 @@ describe('Login functionality', () => {
         cy.get('button[type="submit"]').click();
         cy.contains('is invalid').should('be.visible');
     });
+    it('shouldnt login without password', () =>{
+        cy.visit('https://conduit.mate.academy/');
+        cy.contains('Sign in').click();
+        cy.get('input[type="email"]').type('invalideamil123@gmail.com');
+        cy.get('button[type="submit"]').click();
+        cy.contains('can\'t be blank').should('be.visible');
+    });
+    it('shouldnt login without email', () =>{
+        cy.visit('https://conduit.mate.academy/');
+        cy.contains('Sign in').click();
+        cy.get('input[type="password"]').type('InValiDPassword');
+        cy.get('button[type="submit"]').click();
+        cy.contains('can\'t be blank').should('be.visible');
+    });
 });
