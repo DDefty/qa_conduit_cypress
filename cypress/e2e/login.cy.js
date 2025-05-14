80ec5ca2b2139d6058219b533a1f2ad220a08c0b
DDefty <dawid.kogut99@gmail.com>
2025-05-14 10:49:34 +0200

    Add test case for invalid login credentials in Cypress

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,5 +5,13 @@ describe('Login functionality', () => {
         cy.get('input[type="email"]').type(Cypress.env('email'));
         cy.get('input[type="password"]').type(Cypress.env('password'));
         cy.get('button[type="submit"]').click();
-    })
+    });
+    it('shouldnt login with invalid credentials', () =>{
+        cy.visit('https://conduit.mate.academy/');
+        cy.contains('Sign in').click();
+        cy.get('input[type="email"]').type('invalideamil123@gmail.com');
+        cy.get('input[type="password"]').type('InValiDPassword');
+        cy.get('button[type="submit"]').click();
+        cy.contains('is invalid').should('be.visible');
+    });
 });
