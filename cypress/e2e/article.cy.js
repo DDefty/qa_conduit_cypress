991affb5ad1cee832e6b6b93c3383aebdb86d094
DDefty <dawid.kogut99@gmail.com>
2025-05-18 10:52:59 +0200

    Refactor article test to use article object for improved readability; fix indentation in login command

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,4 +1,12 @@
 describe('Article functionality', () => {
+    const article = {
+        title: 'QA automation enthusiast',
+        description: 'Quality Assurance',
+        body: 'QA  enthusiast',
+        tag: 'QA',
+        updatedBody: 'Quality Assurance entuhusiast'
+    };
+
     beforeEach(() =>{
         cy.visit('https://conduit.mate.academy/');
         cy.contains('Sign in').click();
@@ -6,10 +14,14 @@ describe('Article functionality', () => {
     });
     it('should post an article', () => {
         cy.contains('New Article').click();
-        cy.get('input[placeholder="Article Title"]').clear().type('QA automation enthusiast');
-        cy.get('input[placeholder="What\'s this article about?"').clear().type('QA');
-        cy.get('textarea[placeholder="Write your article (in markdown)"]').clear().type('QA  enthusiast');
-        cy.get('input[placeholder="Enter tags"]').type('QA');
-        cy.contains('Publish Article').click();
+        cy.get('input[placeholder="Article Title"]').clear().type(article.title);
+        cy.get('input[placeholder="What\'s this article about?"').clear().type(article.description);
+        cy.get('textarea[placeholder="Write your article (in markdown)"]').clear().type(article.body);
+        cy.get('input[placeholder="Enter tags"]').type(article.tag);
+        cy.contains('Publish Article').click().click();
+
+        cy.url().should('include', '/article/');
+        cy.contains(article.title);
+        cy.contains(article.tag);
     })
-})
+}) 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,5 @@
 Cypress.Commands.add('login', () => {
-    const username = Cypress.env('username');
+  const username = Cypress.env('username');
   cy.get('input[type="email"]').type(Cypress.env('email'));
   cy.get('input[type="password"]').type(Cypress.env('password'));
   cy.get('button[type="submit"]').click();
